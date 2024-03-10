9ac2e306b0c347c736c799c4f826a18d8215f6ac
hyeonhe <63030323+hyeonhe@users.noreply.github.com>
2024-03-10 11:46:49 +0900

    Time: 43 ms (68.86%), Space: 22.5 MB (6.87%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -5,7 +5,6 @@ class Solution:
             if char.isalnum():
                 arr.append(char.lower())
 
-        print(arr)
         for i in range(len(arr) // 2):
             if arr[i] != arr[len(arr) - i - 1]:
                 return False
